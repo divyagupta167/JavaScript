c5283e6560a0235012b14f646a7126828eb6e4a6
Divya Gupta <54738711+divyagupta167@users.noreply.github.com>
2023-10-05 12:45:13 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all JS code as newer  version of JavaScript
+
+// JavaScripts engine is hidden in browser
+// so alert(3 + 3) works only in browser console and not in node console
+
+// alert("3 + 3")  we are using nodejs and not browser 
+
+// code readability should be high
+
+// ecma script is a javascript standards
+
+
+let name = "divya"
+let age = 23
+let isLogin = false
+let state = null
+
+// we can use typeof to find the datatype of variables
+//Primitive Data Type
+
+// Number => 2 t0 power 53
+// BigInt
+// String
+
+// Boolean => true or false
+// null => standalone value, it is a type of object  
+// undefined => no value is assigned, it is a type of object
+// Symbol => to find uniquesness
+
+//object
+console.log(typeof null);  //object 
+console.log(typeof undefined);  //undefined
+
+
