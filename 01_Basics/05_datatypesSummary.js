c5f7e1cee7c5f012a5c2cca204a5d9eba29fc6de
Divya Gupta <54738711+divyagupta167@users.noreply.github.com>
2023-10-06 16:51:30 +0000

    datatypeSummary

---
+++ 01_Basics/05_datatypesSummary.js
@@ -0,0 +1,34 @@
+//Primitive --> call by value, original reference of memory is not given, copied value is given
+// 7 types :String, Number, Boolean, null, undefined,Symbol(unique),BigInt
+
+// JavaScript is a dynamically typed language. This means that the type of a variable is determined at runtime, not at compile time.
+
+// 100 and 100.3 both comes under number
+
+const temp = null;
+let mail ; // undefined
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id === anotherid); // false
+
+const big = 3837563897409n
+
+// Reference type(Non-Primitive)
+// dataype for non primitive are function, especially for function it is function object
+// Array, Objects, Functions
+
+const heroes = ['thor','wanda', 'panther'];
+let obj = {
+    name:"divya",
+    age:23,
+}
+
+const myfunc = function () {
+    console.log("Hello world");
+}
+
+// use typeof to get type of type of datatypes, null is object type
+
+//datatype of function is function
