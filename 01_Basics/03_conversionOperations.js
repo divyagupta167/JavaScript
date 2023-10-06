329478dffaba6b0a45253386410c299fd806316f
Divya Gupta <54738711+divyagupta167@users.noreply.github.com>
2023-10-06 16:21:37 +0000

    conversionOperations

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -22,6 +22,7 @@ console.log(booleanLOggedIn);  //gives true similarly for 0, gives false
 // Boolean true gives 1 and boolean false gives 0
 
 /*
+
 "33" = 33
 "33abs" = NaN cannot be converted to number
 true = 1; false= 0
@@ -29,3 +30,48 @@ true = 1; false= 0
 "divya"= true
 
 */
+
+
+//        OPEARTIONS              //
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2);
+
+let st1 = "div"
+let st2 = "ya"
+
+let st3 = st1+st2
+console.log(st3);
+
+console.log("1"+2); // 12
+console.log(1+"2"); // 12
+console.log("1"+2+2); // 122
+console.log(1+2+"2"); // 32
+
+
+//precedence
+// Exponentiation - **
+// Multiplication/Division/Remainder - * / / / %
+// Addition/Subtraction - + / -
+
+
+console.log(true); //true
+console.log(+true); // 1
+console.log(true+); //error
+console.log(+""); // 0
+console.log(""+);
+
+
+let num1,num2,num3
+num1=num2=num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
