6a041b91a17451e15d6f37d82701c8918af1201a
Divya Gupta <54738711+divyagupta167@users.noreply.github.com>
2023-10-08 15:34:33 +0000

    objects

---
+++ 02_Basics/04_Objects.js
@@ -0,0 +1,85 @@
+// const User1 = new Object() //singleton object
+// const User2 = {} // non singleton object
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Divya"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+
+//objects inside a objects
+ const regularUser = {
+    email :"some@email.com",
+    fullname:{
+        userFullname:{
+            fisrtname:"divya",
+            lastname:"gupta"
+        }
+    }
+ }
+
+
+ console.log(regularUser.fullname.userFullname.fisrtname);
+//  console.log(regularUser.fullname?.userFullname.fisrtname); here fullname? is used if in any case fullname not present then
+
+//combining of objects
+const obj1 = {
+    1:"a",
+    2:'b',
+    3:'c'
+}
+
+const obj2 = {
+    5:"a",
+    6:'b',
+    7:'c'
+}
+
+// const obj3 = {obj1,obj2} // a new object with two object inside it
+
+const obj3 = Object.assign(obj1,obj2) // a new object in key value pairs
+
+const obj4 = Object.assign({},obj1,obj2) // for more than 2 objects
+// giving an empty object, it will be the target, all the other objects are put into the traget
+
+
+//spread operator
+
+const obj5 = {...obj1,...obj2}
+console.log(obj5)
+
+
+//array objects
+const users = [
+    {
+        id:1,
+        name:'divya'
+    },
+    {
+        id:1,
+        name:'divya'
+    },
+    {
+        id:1,
+        name:'divya'
+    },
+    {
+        id:1,
+        name:'divya'
+    }
+]
+
+
+user[1].email
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
