bb0d6dd6841cf39c1fdac62188246b876573414f
Divya Gupta <54738711+divyagupta167@users.noreply.github.com>
2023-10-09 18:12:24 +0000

    code execution

---
+++ 03_Basics/05_CodeExecution.js
@@ -0,0 +1,20 @@
+// GE is refered to a variable this
+// every environment has different value of this
+// node has different and browser has different
+// for browser, this point window object
+
+/*
+ 
+
+Javascript Execution context
+1. global ec
+2. function ec
+3. eval ec
+
+
+phases --> 1. memory creation phase
+           2. execution phase
+*/ 
+
+//call stack is created
+// we can see in windows-> inspect-> sources-> new snippet
