9cc14ab8ef16e1951b82db82597efa1a35968589
Divya Gupta <54738711+divyagupta167@users.noreply.github.com>
2023-10-09 16:53:36 +0000

    function

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -35,3 +35,48 @@ function loginMessage(name="sam") { //sam is deafult, if no value entered
 }
 
 console.log(loginMessage("divya"));
+
+function price(...num1){ // here ... refers as "rest-operator", returns an array
+    return num1
+}
+// console.log(price(8));
+console.log(price(8,8,9,5,6,7));
+
+function price(val1, val2, val3,...num1){  // first 3 goes to variables and rest to an array
+    return num1
+}
+
+
+//Object with function
+const userName = {
+    name :"divya",
+    id:1
+}
+
+function handleObject(anyobj) {
+    console.log(`User name is ${anyobj.userName} and 
+    price is ${anyobj.price}`);
+}
+// handleObject(userName)
+//OR
+handleObject({
+    name :"divya",
+    id:1
+})
+
+
+//Arrays and function
+const mYArr = [200,400,600,700,898]
+
+function returnValue(getArr) {
+    return getArr[1]
+}
+
+
+// console.log(returnValue(mYArr));
+//OR
+console.log(returnValue([1,2,3,4,5,6,7]));
+
+
+
+//Function Scope
