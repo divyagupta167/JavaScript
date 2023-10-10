1aa7460fb513c126e77f85bbbfc53304bf129d17
Divya Gupta <54738711+divyagupta167@users.noreply.github.com>
2023-10-10 10:37:18 +0000

    reduces

--- 05_Iterations/eight.js
+++ 05_Iterations/eight.js
@@ -5,3 +5,24 @@ const myTotal = myNums.reduce((acc, currVal)=>{
 }, 0)
 
 console.log(myTotal);
+
+//eg
+
+const shopping = [
+    {
+        name : 'a',
+        price : 600
+    },
+    {
+        name : 'b',
+        price : 200
+    },
+    {
+        name : 'c',
+        price : 9600
+    },
+]
+
+const total = shopping.reduce((acc,item)=> acc + item.price, 0)
+
+console.log(total);
