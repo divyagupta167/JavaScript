fc1cc5386108481ed3973d6df7b3df11e7c0c814
Divya Gupta <54738711+divyagupta167@users.noreply.github.com>
2023-10-10 09:23:57 +0000

    while and do while

---
+++ 05_Iterations/two.js
@@ -0,0 +1,21 @@
+
+// while loop
+let index = 0
+while (index <= 10) {
+    console.log(`${index}`);
+    index =+ 2
+}
+
+let myArray = ["f","b","s"]
+let i = 0
+while (i < myArray.length) {
+    console.log(`${myArray[i]}`);
+    i +=1
+}
+
+//do while
+let score = 1
+do {
+    console.log(`${score}`);
+    score ++
+} while (score <=  10);
