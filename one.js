0a1fb613eb90297d3efd90ddcf8760d1af02a22a
Divya Gupta <54738711+divyagupta167@users.noreply.github.com>
2023-10-10 16:52:39 +0000

    async

---
+++ one.js
@@ -0,0 +1,25 @@
+/*
+
+//Default Javascript
+Javascript 
+        --> synchronous and single threaded
+
+// Each operation waits for the last one to 
+complete before execution
+Execution context
+        --> execute one line of code at a time
+        --> console log 1
+        --> console log 2
+        --> CALL STACK     MEMORY HEAP
+
+//Blocking Code Vs Non-Blocking code
+
+1.Blocking -> block the flow of program(read file Sync)
+2.Non-Blocking ->does not block the flow of program(Read file Async)
+
+
+
+
+
+
+*/
